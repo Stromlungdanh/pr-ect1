5db558718287ecf3a5466fa51ea078c9a90c3057
Stromlungdanh <quannguyen2567@gmail.com>
2020-11-06 20:04:35 +0700

    date

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (
+      <div className="App">
+        <h1>antd version: {version}</h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
+      </div>
+    )
+    
   );
 }
 
